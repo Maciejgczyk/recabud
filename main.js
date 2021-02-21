426d5c577b725c79270b9fd6aaad226fdd7e2963
Maciej Grzegorczyk <macieku00@gmail.com>
2021-02-21 16:56:29 +0100

    cmt

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ const hamburgerMenu = document.querySelector('.hamburger');
 const hamburgerSite = document.querySelector('.hamburger-menu');
 const closeButton = document.querySelector('.close-button');
 
+const hamburgerLinks = document.querySelectorAll('.hamburger-wrap-a');
+
 hamburgerMenu.addEventListener('click', () => {
     hamburgerSite.classList.toggle('hamburger-menu-active');
 })
 closeButton.addEventListener('click', () => {
     hamburgerSite.classList.toggle('hamburger-menu-active');
+})
+
+hamburgerLinks.addEventListener('click', (hamburgerLink) => {
+    hamburgerLinks.forEach(hamburgerLink => {
+        hamburgerSite.classList.toggle('hamburger-menu-active');
+    });
 })
